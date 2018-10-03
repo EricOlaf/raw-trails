5a546dd9097179229eeb2acfb1be5960284f2519
Eric Olaveson <olavesoneric@gmail.com>
2018-10-03 16:37:37 -0500

    returned video

--- src/components/VideoPage/VideoPage.js
+++ src/components/VideoPage/VideoPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import YouTube from "react-youtube";
 // import { Player } from "video-react";
 // import video from "../../assets/video/Yosemite JMT 1min.mov";
 // import picVid from "../../assets/pics/1.jpg";
@@ -8,13 +9,26 @@ import "./VideoPage.css";
 
 class VideoPage extends Component {
   render() {
+    const opts = {
+      height: "468",
+      width: "768",
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
     return (
       <div>
         <div className="videoContainer">
           {/* <Player playsInline poster={picVid} src={video} /> */}
+          <YouTube videoId="FHWypyEY1zM" opts={opts} onReady={this._onReady} />
         </div>
       </div>
     );
+  }
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
   }
 }
 
