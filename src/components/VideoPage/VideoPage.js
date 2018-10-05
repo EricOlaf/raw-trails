7e35f0237ae0a3cca9e7beeea646e315160cf16b
Eric Olaveson <olavesoneric@gmail.com>
2018-10-04 20:58:34 -0500

    put video back in

--- src/components/VideoPage/VideoPage.js
+++ src/components/VideoPage/VideoPage.js
@@ -1,34 +1,25 @@
 import React, { Component } from "react";
-import YouTube from "react-youtube";
-// import { Player } from "video-react";
-// import video from "../../assets/video/Yosemite JMT 1min.mov";
-// import picVid from "../../assets/pics/1.jpg";
+
+import ReactPlayer from "react-player";
 
 import "./VideoPage.css";
-// import "../../../node_modules/video-react/dist/video-react.css";
 
 class VideoPage extends Component {
   render() {
-    const opts = {
-      height: "468",
-      width: "768",
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 1
-      }
-    };
     return (
       <div>
         <div className="videoContainer">
-          {/* <Player playsInline poster={picVid} src={video} /> */}
-          <YouTube videoId="FHWypyEY1zM" opts={opts} onReady={this._onReady} />
+          <ReactPlayer
+            url="https://res.cloudinary.com/rawtrails801/video/upload/v1538704262/Yosemite_JMT_small.mp4"
+            className="react-player"
+            playing="false"
+            width="100%"
+            height="100%"
+            controls="true"
+          />
         </div>
       </div>
     );
-  }
-  _onReady(event) {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
   }
 }
 
