834ae418b296e0eda9f41494b8da1d5075d39988
Eric Olaveson <olavesoneric@gmail.com>
2019-02-22 09:10:24 -0600

    video changes

--- src/components/VideoPage/VideoPage.js
+++ src/components/VideoPage/VideoPage.js
@@ -6,17 +6,18 @@ import "./VideoPage.css";
 
 class VideoPage extends Component {
   render() {
+    const nope = false;
     return (
       <div>
         <div className="videoContainer">
           <ReactPlayer
             url="https://res.cloudinary.com/rawtrails801/video/upload/v1538704262/Yosemite_JMT_small.mp4"
             className="react-player"
-            playing="false"
+            playing={false}
             width="100%"
             height="100%"
             controls="true"
-            autoPlay="false"
+            autoPlay={false}
           />
         </div>
       </div>
